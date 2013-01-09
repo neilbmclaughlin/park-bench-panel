9b3b1a79cf166d595226adcc7403bf5258b0cb90
OpenShift guest <neil@mittenview.co.uk>
2013-01-09 16:11:42 -0500

    added script to generate the hangout xml file from park-bench-panel.html. this will need to be run before each commit

---
+++ generate-xml.sh
@@ -0,0 +1,11 @@
+awk 'BEGIN {
+  print "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+  print "<Module>"
+  print "  <ModulePrefs title=\"Hangout Starter\">"
+  print "    <Require feature=\"rpc\" />"
+  print "    <Require feature=\"views\" />"
+  print "    <Require feature=\"locked-domain\" />"
+  print "  </ModulePrefs>"
+  print "  <Content type=\"html\"><![CDATA[ "}
+    { print $0 }
+END {print "]]></Module>"}' park-bench-panel.html
