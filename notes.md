495e06295318e5386c6905e2ee9097201cad3655
OpenShift guest <neil@mittenview.co.uk>
2013-01-10 20:05:22 -0500

    changed to handle both hangout and jquery load, load participants on document ready

--- notes.md
+++ notes.md
@@ -4,5 +4,6 @@ Notes & Links
 * [Hangouts console](https://code.google.com/apis/console/b/0/#project:727799527310)
 * [Github repo](https://github.com/neilbmclaughlin/park-bench-panel)
 * [Cloud 9 Project](http://c9.io/neilbmclaughlin/park-bench-panel)
+* [Demo app walkthrough](http://life.scarygami.net/hangoutapps/)
 
 
--- park-bench-panel.js
+++ park-bench-panel.js
@@ -1,57 +1,33 @@
-function showParticipants_old() {
-  var participants = gapi.hangout.getParticipants();
-
-  var retVal = '<p>Participants: </p><ul>';
-
-  for (var index in participants) {
-    var participant = participants[index];
-
-    if (!participant.person) {
-      retVal += '<li>A participant not running this app</li>';
-    }
-    retVal += '<li>' + participant.person.displayName + '</li>';
-  }
-
-  retVal += '</ul>';
-
-  var div = document.getElementById('participants');
-
-  div.innerHTML = retVal;
-}
-
 function showParticipants() {
-  var items=[];
-  var participants = gapi.hangout.getParticipants();
+    var items = [];
+    var participants = gapi.hangout.getParticipants();
 
-  $(participants).each(function(index, Element) {
-      items.push($('<li/>').text(Element.person.displayName));
-  });
-  $('#participantList').append.apply($('#participantList'), items);
-  
-  $( "#participantList li" ).draggable({
-    appendTo: "body",
-    helper: "clone"
-  });
-  
-  $( "#speakerList" ).droppable({
-    activeClass: "ui-state-default",
-    hoverClass: "ui-state-hover",
-    drop: function( event, ui ) {
-        $( this ).find( ".speakerPlaceholder" ).remove();
-        $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
-    }
-})
-  
-}
+    $(participants).each(function(index, Element) {
+        items.push($('<li/>').text(Element.person.displayName));
+    });
+    $('#participantList').append.apply($('#participantList'), items);
 
-function init() {
-  gapi.hangout.onApiReady.add(
-      function(eventObj) {
-        if (eventObj.isApiReady) {
-          document.getElementById('showParticipants')
-            .style.visibility = 'visible';
+    $("#participantList li").draggable({
+        appendTo: "body",
+        helper: "clone"
+    });
+
+    $("#speakerList").droppable({
+        activeClass: "ui-state-default",
+        hoverClass: "ui-state-hover",
+        drop: function(event, ui) {
+            $(this).find(".speakerPlaceholder").remove();
+            $("<li></li>").text(ui.draggable.text()).appendTo(this);
         }
-      });
+    });
 }
 
-gadgets.util.registerOnLoadHandler(init);
+$(document).ready(function() {
+       if(gapi.hangout.isApiReady()){ 
+            console.log("Yes it was ready. We can start."); 
+            showParticipants();
+        } else { 
+            console.log("No - not read yet. We have to listen."); 
+            gapi.hangout.onApiRead.add(showParticipants());
+        }
+});
