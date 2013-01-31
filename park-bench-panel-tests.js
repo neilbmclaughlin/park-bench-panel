c1cabb3ebe47a51cbd1d54c208de828f07199e50
OpenShift guest <neil@mittenview.co.uk>
2013-01-31 08:09:10 -0500

    added to qUnit fixture and created new test for ul poulation

--- park-bench-panel-tests.js
+++ park-bench-panel-tests.js
@@ -1,3 +1,30 @@
-test("Participant unordered list is created", function() {
+test("Dummy Test", function() {
     equal(aMethod(), "boo");
+});
+
+test("Participant unordered list is created", function() {
+    
+    //arrange
+    var pList = {
+        person : {
+            displayName : 'Bob'
+        },
+        person : {
+            displayName : 'Fred'
+        },    
+    }
+    
+    //act
+    buildParticipantList(pList);
+        
+    //assert
+    var listItems = document
+        .getElementById("qunit-fixture")
+        .getElementById("participantList")
+        .getElementsByTagName("li");
+    
+    
+    equal(listItems.length, 2);
+    equal(listItems[0].innerHTML, "Bob");
+    equal(listItems[1].innerHTML, "Bill");
 });
