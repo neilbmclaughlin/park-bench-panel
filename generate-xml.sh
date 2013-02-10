99864f4799ce5ccc09bc1c6708345c676798d93f
OpenShift guest <neil@mittenview.co.uk>
2013-02-10 18:45:08 -0500

    created script to build dev and int versions of test fixture files

--- generate-xml.sh
+++ generate-xml.sh
@@ -1,7 +1,7 @@
 sed -e '/PBP SCRIPT BLOCK START/r script-fragment-int.html' park-bench-panel-template.html > park-bench-panel.html
 sed -e '/CDATA/r park-bench-panel.html' park-bench-panel-template.xml >  park-bench-panel-int.xml                                                                                         
-sed -e '/PBP SCRIPT BLOCK START/r script-fragment-int.html' park-bench-panel-tests-template.html > park-bench-panel-tests-int.html
+sed -e '/PBP SCRIPT BLOCK START/r script-fragment-test-int.html' park-bench-panel-tests-template.html > park-bench-panel-tests-int.html
 
 sed -e '/PBP SCRIPT BLOCK START/r script-fragment-dev.html' park-bench-panel-template.html > park-bench-panel.html
 sed -e '/CDATA/r park-bench-panel.html' park-bench-panel-template.xml >  park-bench-panel-dev.xml
-sed -e '/PBP SCRIPT BLOCK START/r script-fragment-dev.html' park-bench-panel-tests-template.html > park-bench-panel-tests-dev.html
+sed -e '/PBP SCRIPT BLOCK START/r script-fragment-test-dev.html' park-bench-panel-tests-template.html > park-bench-panel-tests-dev.html
