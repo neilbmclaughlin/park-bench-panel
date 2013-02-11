868710fe981887bdf449a23e36cbf02a1d3a151d
OpenShift guest <neil@mittenview.co.uk>
2013-02-11 18:03:42 -0500

    had not saved thi file for the previous commit

--- park-bench-panel.js
+++ park-bench-panel.js
@@ -65,18 +65,3 @@ var parkBenchPanel = function(hangout) {
     }
     
 };
-
-var hangout = new hangoutWrapper();    
-var pbp = new parkBenchPanel(hangout);
-
-$(document).ready(function() {
-    pbp.init();
-});
-
-function startTalk() {
-    pbp.startTalk(hangout.getLocalParticipant());
-}
-
-function stopTalk() {
-    pbp.stopTalk(hangout.getLocalParticipant());
-}
