41e5f20a9a79f6f4c06e8dfc77c64d22194064f9
OpenShift guest <neil@mittenview.co.uk>
2013-02-10 19:08:33 -0500

    update of links

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-if [[ ! -a ./park-bench-panel.html ]] 
-then
-  echo "Error: park-bench-panel.html must exist"
-  exit 1
-fi
+./generate-xml.sh
+git add .
 
-git diff --cached --quiet ./park-bench-panel.html
-
-STATUS=$?
-
-if [[ $STATUS -eq 1 ]]
-then
-  ./generate-xml.sh
-  git add ./park-bench-panel.xml
-  echo "park-bench-panel.xml was regenerated."
-else
-  echo "park-bench-panel.xml is up to date."
-fi
-
+#
+#if [[ ! -a ./park-bench-panel.html ]] 
+#then
+#  echo "Error: park-bench-panel.html must exist"
+#  exit 1
+#fi
+#
+#git diff --cached --quiet ./park-bench-panel.html
+#
+#STATUS=$?
+#
+#if [[ $STATUS -eq 1 ]]
+#then
+#  ./generate-xml.sh
+#  git add ./park-bench-panel.xml
+#  echo "park-bench-panel.xml was regenerated."
+#else
+#  echo "park-bench-panel.xml is up to date."
+#fi
+#
