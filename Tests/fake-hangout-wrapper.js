8b24e5c7262a3822e53fe6d73f9b4084328b80a6
OpenShift guest <neil@mittenview.co.uk>
2013-02-19 19:29:45 -0500

    more work to get fake-hangout-wrapper up and running

--- Tests/fake-hangout-wrapper.js
+++ Tests/fake-hangout-wrapper.js
@@ -1,27 +1,29 @@
 function hangoutWrapper() {
 
     var participants = [{
+        id: 1,
         person: {
             displayName: 'Bob',
         },
         status: 'listener'
 
     }, {
+        id: 2,
         person: {
             displayName: 'Fred',
         },
         status: 'listener'
     }, {
+        id: 3,
         person: {
             displayName: 'Bill',
         },
         status: 'listener'
     }];
 
-
-
     var that = this;
     var newParticipantCallBack = null;
+    var stateChangedCallBack = null;
 
     this.getParticipants = function() {
 
@@ -36,16 +38,27 @@ function hangoutWrapper() {
     this.addOnApiReadyCallback = function(f) {}
 
     this.getLocalParticipant = function() {
-        return participants[0];
+        return jQuery.grep(participants, function(p){
+            return (p.person.displayName == $('#localParticipant').val() );
+        })[0];
     }
 
     this.addOnNewParticipantCallback = function(f) {
         this.newParticipantCallBack = f
     }
 
-    this.addOnStateChangedCallback = function(f) {}
+    this.addOnStateChangedCallback = function(f) {
+        this.stateChangedCallBack = f        
+    }
 
-    this.setParticipantAsSpeaker = function(participantId) {}
+    this.setParticipantAsSpeaker = function(delta) {
+        jQuery.each(participants, function(i, p){
+            if ( delta[p.id] != undefined ) {
+                p.status = 'speaker'
+            }
+        });
+        that.stateChangedCallBack(null);
+    }
 
     this.setParticipantAsListener = function(participantId) {}
 
@@ -55,8 +68,9 @@ function hangoutWrapper() {
 
     this.addTestParticipant = function() {
         var p = {
+            id : $('#participantId').val() * 1,
             person: {
-                displayName: $('#displayname').val(),
+                displayName : $('#displayName').val(),
             },
             status: 'listener'
         };
